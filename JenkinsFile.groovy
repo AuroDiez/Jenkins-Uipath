f4664ba6f4cc577cb76b569ac65b5b7f1f6b6a97
Aurora Diez Herrero <aurora.diez.herrero@uipath.com>
2025-04-09 00:30:21 +0200

    commit3

--- JenkinsFile.groovy
+++ JenkinsFile.groovy
@@ -7,9 +7,9 @@ pipeline {
 	        MAJOR = '1'
 	        MINOR = '0'
 	        //Orchestrator Services
-	        UIPATH_ORCH_URL = "https://staging.uipath.com/"
-	        UIPATH_ORCH_LOGICAL_NAME = "emeaeese"
-	        UIPATH_ORCH_TENANT_NAME = "LabAuro"
+	        UIPATH_ORCH_URL = "https://cloud.uipath.com/"
+	        UIPATH_ORCH_LOGICAL_NAME = "dilipejzisxn"
+	        UIPATH_ORCH_TENANT_NAME = "DefaultTenant"
 	        UIPATH_ORCH_FOLDER_NAME = "Shared"
 			UIPATH_PROCESS_NAME = "JenkinsTestProcess"
 	    }
